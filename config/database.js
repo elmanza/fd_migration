02f355435793ffccca6ef315e8c2fbc0f9f7bfc4
Harold Alexander Gallardo M <hgallardo@lean-tech.io>
2020-01-14 00:41:09 -0500

    disable log on db

--- config/database.js
+++ config/database.js
@@ -16,7 +16,7 @@ function getSequelizeConnection(params){
                 acquire: 30000,
                 idle: 10000
             },
-            logging: true
+            logging: false
         }
     );
 
