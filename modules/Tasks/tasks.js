58e93b9c4e6c84153df171188c1ce82988ca538f
Harold Alexander Gallardo M <hgallardo@lean-tech.io>
2020-01-23 13:46:00 -0500

    Test create quotes

--- modules/Tasks/tasks.js
+++ modules/Tasks/tasks.js
@@ -7,22 +7,28 @@ quoteTask = new QuoteTask();
 memberTask = new MemberTask();
 
 module.exports = {
-    getMembersList: function(){
+    /* getMembersList: function(){
+        console.log("getMembersList task is called........................");
         memberTask.getList();
-    },
+    }, */
     createQuotes: function(){
+        console.log("createQuotes task is called........................");
         quoteTask.createQuotes();
-    },
+    },/* 
     refreshQuote: function(){
+        console.log("refreshQuote task is called........................");
         quoteTask.refreshQuotes();
     },
     quotesToOrders: function(){
+        console.log("quotesToOrders task is called........................");
         quoteTask.quotesToOrders();
     },
     refreshOrders: function(){
+        console.log("refreshOrders task is called........................");
         orderTask.refreshOrders();
     },
     sendOrderNotes: function(){
+        console.log("sendOrderNotes task is called........................");
         orderTask.sendOrderNotes();
-    }
+    } */
 }
