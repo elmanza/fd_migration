232c897ffb053a1f2020ace3382facb8fe71072d
Harold Alexander Gallardo <hgallardo@lean-tech.io>
2020-07-21 22:59:07 -0500

    Fixed

--- modules/tasks/index.js
+++ modules/tasks/index.js
@@ -5,9 +5,9 @@ FDTasks = new FreightDragonMigrationTasks();
 RWTasks = new RWSynchronizatorTasks();
 
 module.exports = {
-   /*  migrate(){
+    /* migrate(){
         return FDTasks.migrateAll();
-    },
+    }, */
     migrateTodayEntities(){
         return FDTasks.migrateTodayEntities();
     },
@@ -16,7 +16,7 @@ module.exports = {
     },
     quoteToOrder(){
         return RWTasks.quoteToOrder();
-    }, */
+    },
     refreshEntities(){
         return RWTasks.refreshEntities();
     }
